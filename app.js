905cd8c18f2d486906c6be9bbe963dec374bfeb1
Gyaneshwar Kumar <codewithgyan@gmail.com>
2023-04-14 00:40:11 +0530

    complete

--- app.js
+++ app.js
@@ -1,41 +1,35 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
-
-app.use(logger('dev'));
+const express = require("express");
+const cors = require('cors');
+const multipart = require('connect-multiparty')
+const app = express();
+const multipartMiddleware = multipart();
+const { 
+    login, 
+    signup,
+    getSchoolDetails,
+    getAllSchools,
+    editTeacher,
+    editStudent 
+} = require("./controllers/user.controller");
+const {addNewClass} = require("./controllers/class.controller");
+const {addNewCourse} = require("./controllers/course.controller");
+const protect = require("./middleware/protect");
+const { fileUpload, fileDownload } = require("./controllers/file.controller");
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use("/get-all-schools",getAllSchools);
+app.use("/get-school-details",getSchoolDetails);
+app.use("/edit-teacher",protect,editTeacher);
+app.use("/edit-student",protect,editStudent);
+app.use("/login", login);
+app.use("/signup", signup);
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/class/add",addNewClass);
+app.use("/course/add",addNewCourse);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
-
+// File Upload
+app.use("/upload",multipartMiddleware,fileUpload);
+app.use("/download",fileDownload);
 module.exports = app;
